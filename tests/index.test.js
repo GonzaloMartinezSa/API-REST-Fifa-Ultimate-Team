b089f89a439a9e61d8fc8dd1e908c0d41f41954b
GonzaloMartinezSa <gmartnezsarmiento@frba.utn.edu.ar>
2023-03-21 18:13:21 -0300

    Sumo base para tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,14 +1,16 @@
-import app from "../src/app";
-import request from "supertest";
+
+import app from '../src/app.js';
+import request from 'supertest';
 
 describe("Index Routes", () => {
-  it("should respond welcome", async () => {
+
+  test("should respond welcome", async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toEqual(200);
     expect(res.body).toEqual({ message: "welcome to my api" });
   });
 
-  it("should respond pong", async () => {
+  test("should respond pong", async () => {
     const res = await request(app).get("/ping");
     expect(res.statusCode).toEqual(200);
     expect(res.body).toEqual({ result: "pong" });
